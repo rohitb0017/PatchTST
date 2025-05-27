79a5bb6d2520e892dc8e5b06fa651a915cc6d1de
Rohit <rohitbasu8953@gmail.com>
2025-05-27 22:18:44 +0530

    Update etth1.sh

--- PatchTST_supervised/scripts/PatchTST/etth1.sh
+++ PatchTST_supervised/scripts/PatchTST/etth1.sh
@@ -16,7 +16,7 @@ data_name=ETTh1
 random_seed=2021
 for pred_len in 96 192 336 720
 do
-    python -u run_longExp.py \
+    python -u /kaggle/working/PatchTST/PatchTST_supervised/run_longExp.py \
       --random_seed $random_seed \
       --is_training 1 \
       --root_path $root_path_name \
