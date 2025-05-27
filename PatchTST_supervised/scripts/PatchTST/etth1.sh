520eca3d4248dbe08d5b13ec4087366563b9dea4
Rohit <rohitbasu8953@gmail.com>
2025-05-27 23:41:58 +0530

    Update etth1.sh

--- PatchTST_supervised/scripts/PatchTST/etth1.sh
+++ PatchTST_supervised/scripts/PatchTST/etth1.sh
@@ -38,6 +38,6 @@ do
       --patch_len 16\
       --stride 8\
       --des 'Exp' \
-      --train_epochs 1\
+      --train_epochs 10\
       --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
 done
