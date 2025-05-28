2f3b21b67c0443dbe2400550295f84ccb2e24f7f
Rohit <rohitbasu8953@gmail.com>
2025-05-29 00:27:38 +0530

    Update run_longExp.py

--- PatchTST_supervised/run_longExp.py
+++ PatchTST_supervised/run_longExp.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('--features', type=str, default='M',
                         help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
     parser.add_argument('--target', type=str, default='Volume', help='target feature in S or MS task')
-    parser.add_argument('--freq', type=str, default='h',
+    parser.add_argument('--freq', type=str, default='d',
                         help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
     parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
 
