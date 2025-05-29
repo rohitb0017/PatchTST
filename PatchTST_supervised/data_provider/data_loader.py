3a44b5afba77f7b673dac30f7b638f0f4faae99c
Rohit <rohitbasu8953@gmail.com>
2025-05-29 12:28:07 +0530

    Update data_loader.py
    changed Dataset_pred class:
    1. dataset path
    2. changing 'date' to 'Date' at every possible place

--- PatchTST_supervised/data_provider/data_loader.py
+++ PatchTST_supervised/data_provider/data_loader.py
@@ -294,8 +294,8 @@ class Dataset_Custom(Dataset):
 
 class Dataset_Pred(Dataset):
     def __init__(self, root_path, flag='pred', size=None,
-                 features='S', data_path='ETTh1.csv',
-                 target='OT', scale=True, inverse=False, timeenc=0, freq='15min', cols=None):
+                 features='M', data_path='AMAZON.csv',
+                 target='Volume', scale=True, inverse=False, timeenc=0, freq='d', cols=None):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
@@ -322,8 +322,9 @@ class Dataset_Pred(Dataset):
 
     def __read_data__(self):
         self.scaler = StandardScaler()
-        df_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.data_path))
+        #df_raw = pd.read_csv(os.path.join(self.root_path,
+                                          #self.data_path))
+        df_raw = pd.read_csv('/content/PatchTST/dataset/AMAZON.csv')
         '''
         df_raw.columns: ['date', ...(other features), target feature]
         '''
@@ -333,7 +334,7 @@ class Dataset_Pred(Dataset):
         else:
             cols = list(df_raw.columns)
             cols.remove(self.target)
-            cols.remove('date')
+            cols.remove('Date')
         df_raw = df_raw[['date'] + cols + [self.target]]
         border1 = len(df_raw) - self.seq_len
         border2 = len(df_raw)
@@ -350,11 +351,11 @@ class Dataset_Pred(Dataset):
         else:
             data = df_data.values
 
-        tmp_stamp = df_raw[['date']][border1:border2]
-        tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
+        tmp_stamp = df_raw[['Date']][border1:border2]
+        tmp_stamp['Date'] = pd.to_datetime(tmp_stamp.Date)
         pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len + 1, freq=self.freq)
 
-        df_stamp = pd.DataFrame(columns=['date'])
+        df_stamp = pd.DataFrame(columns=['Date'])
         df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
         if self.timeenc == 0:
             df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
@@ -365,7 +366,7 @@ class Dataset_Pred(Dataset):
             df_stamp['minute'] = df_stamp.minute.map(lambda x: x // 15)
             data_stamp = df_stamp.drop(['date'], axis=1).values
         elif self.timeenc == 1:
-            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            data_stamp = time_features(pd.to_datetime(df_stamp['Date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
 
         self.data_x = data[border1:border2]
